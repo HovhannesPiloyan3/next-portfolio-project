96e39ba2285011661fc0e435107c03e0bbe05c18
HovoPiloyan3 <hovo.piloyan3@mail.ru>
2025-02-26 14:19:55 +0400

    changed main page add icons and bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-
+import { navItems } from "@/data";
 import Hero from "@/components/Hero/Hero";
-
+import {FloatingNav} from "@/components/ui/FloatingNavbar";
+import Grid from "@/components/Grid/Grid";
 export default function Home() {
-  return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero/>
-      </div>
-    </main>
-      );
+    return (
+        <main
+            className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <div className="max-w-7xl w-full">
+                <FloatingNav navItems={navItems} />
+                <Hero/>
+                <Grid/>
+            </div>
+        </main>
+    );
 }
